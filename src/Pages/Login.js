bb03a7c2add161dd089baf6db6512a241e303118
Riccardo <riccardo.dipaola22@gmail.com>
2023-05-31 15:20:50 +0200

    implementato login funzionante

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,22 +1,52 @@
 import React, {useState} from "react";
+import swal from 'sweetalert'
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:4000/api/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+   }
+
 const Login = (props) => {
 
   
     const [username, setUsername] = useState ('');
     const [password, setPassword] = useState ('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        console.log(username);
-    }
+        const response = await loginUser({
+          username,
+          password
+        });
+        if ('token' in response) {
+          swal("Success", "Good", "success", {
+            buttons: false,
+            timer: 2000,
+          })
+          .then((value) => {
+            localStorage.setItem('token', response['token']);
+            localStorage.setItem('user', JSON.stringify(response['user']));
+            window.location.href = "/";
+          });
+        } else {
+          swal("Failed", "Wrong", "error");
+        }
+      }
+
     return (
         <div className="auth-form-container">
             <h2>Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="username">Username</label>
-            <input vale ={username} type="username" placeholder="Username" id="username" name="username" />
+            <input vale ={username} type="username" placeholder="Username" id="username" name="username" onChange={e => setUsername(e.target.value)} />
             <label htmlFor="password">Password</label>
-            <input vale ={password} type="password" placeholder="xxxxxxxxxx" id="password" name="password" />
+            <input vale ={password} type="password" placeholder="xxxxxxxxxx" id="password" name="password" onChange={e => setPassword(e.target.value)} />
             <button type="submit">Log In</button>
         </form>
         </div>
